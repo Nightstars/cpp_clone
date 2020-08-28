dd4075704eed3a207e36197248a535fb6375e587
Nightstars <christzhangowner@gmail.com>
2020-08-28 21:37:21 +0800

    添加项目文件。

---
+++ cpp_clone/Teacher.cpp
@@ -0,0 +1,21 @@
+#include "Teacher.h"
+Teacher::Teacher(string _name, int _age) {
+	cout << "Teacher::Teacher(string _name, int _age)" << endl;
+	m_strName = _name;
+	m_iAge = _age;
+}
+void Teacher::setName(string _name) {
+	m_strName = _name;
+}
+string Teacher::getName() {
+	return m_strName;
+}
+void Teacher::setAge(int _age) {
+	m_iAge = _age;
+}
+int Teacher::getAge() {
+	return m_iAge;
+}
+Teacher::Teacher(const Teacher& tea) {
+	cout << "Teacher::Teacher(const Teacher& tea)" << endl;
+}
